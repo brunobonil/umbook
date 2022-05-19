34ecd2b5dc1658a23c6b71623bff8c4fc831d0e8
jlocamuz <jlocamuz@gmail.com>
2022-05-19 15:55:07 -0300

    Register - front

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
 
     # Custom apps
     'umbook',
@@ -65,9 +67,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
+
+# Corsheaders
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 TEMPLATES = [
     {
