bbd2b82fbc891e5bb87832b3c00300ce15b97cf5
brunobonil <brunobonil@gmail.com>
2022-05-22 22:56:33 -0300

    Estructura para subir foto

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,6 @@
 import Login from "./Login";
 import Register from "./Register";
+import UploadPhoto from "./UploadPhoto";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
       <Router>
         <Routes>     
           <Route exact path="/" element={<Login/>}/>  
-          <Route exact path="/register/" element={<Register/>}/>    
+          <Route exact path="/register/" element={<Register/>}/>   
+          <Route exact path="/upload-photo" element={<UploadPhoto/>}/>  
         </Routes>
       </Router>
     </div>
